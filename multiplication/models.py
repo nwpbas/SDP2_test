0085a4f4d7afbc68bce6a17f4b00d56df279bca7
Bas Nawaphat <s5801012610075@email.kmutnb.ac.th>
2019-02-14 15:19:04 +0700

    show multiplication of inpute type textbox from

--- multiplication/models.py
+++ multiplication/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Multiplication(models.Model):
+    number = models.PositiveIntegerField()
+    count = models.PositiveIntegerField(default=1)
--- multiplication/views.py
+++ multiplication/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Multiplication
 # Create your views here.
+
+def index(request):
+    context = {}
+    if "number" in request.GET:
+        if request.GET['number'] != '':
+
+            if not Multiplication.objects.filter(number=int(request.GET['number'])):
+                obj = Multiplication(number=int(request.GET['number']))
+                obj.save()
+                context['count'] = Multiplication.objects.filter(number=int(request.GET['number'])).count
+            else:
+                obj = Multiplication.objects.get(number=int(request.GET['number']))
+                obj.count += 1
+                obj.save()
+            context['multipliction_obj'] = obj
+            result = [int(request.GET['number'])*i for i in range(1,13)]
+            context['result'] = result
+    return render(request, "multiplication/index.html",context)
+    
+    
